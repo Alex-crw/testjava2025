a8044953ea039f60bb0d1cabf9b7c18991056074
Alejandro Garcia Hernandez <alejandro.inves@gmail.com>
2025-09-02 16:38:22 +0200

    Initial commit

---
+++ TestJava2025/src/test/java/com/alexgrchdez/testjava2025/TestJava2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexgrchdez.testjava2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJava2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
